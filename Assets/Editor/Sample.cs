ddac315af7788fb792bef8a106ba5f0f5cd782c5
tylearymf <tylearymf@163.com>
2019-01-25 11:53:39 +0800

    Add Project

---
+++ Assets/Editor/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Test
+{
+    [MenuItem("Tools/Test")]
+    static void A()
+    {
+        Debug.Log(DateTime.Now);
+        EditorCoroutine.StartCoroutine(new EditorWaitForSeconds(3, () =>
+        {
+            Debug.Log(DateTime.Now);
+        }));
+    }
+}
